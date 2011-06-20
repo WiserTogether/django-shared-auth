1294c40a086d54ae3657aedc603c96974fd5e9a8
Viraj Kanwade <viraj.kanwade@snstech.com>
2011-06-20 15:46:47 +0530

    1. Added logger statements 2. set request.session['SHARED_AUTH_USER'] after login and check it before logging out user.

--- django_shared_auth/middleware.py
+++ django_shared_auth/middleware.py
@@ -23,34 +23,48 @@ class SharedAuthConsumerMiddleware(object):
         )
     """
     def process_request(self, request):
-        if request.COOKIES.has_key(settings.COOKIE_NAME):
-            cookie_str = request.COOKIES.get(settings.COOKIE_NAME)
-            setattr(request, settings.COOKIE_NAME, cookie_str)
-            # If the user is already authenticated and that user is the user we are
-            # getting passed in the headers, then the correct user is already
-            # persisted in the session and we don't need to continue.
+        try:
+            logger.debug("processing request...")
+            if request.COOKIES.has_key(settings.COOKIE_NAME):
+                cookie_str = request.COOKIES.get(settings.COOKIE_NAME)
+                setattr(request, settings.COOKIE_NAME, cookie_str)
+                # If the user is already authenticated and that user is the user we are
+                # getting passed in the headers, then the correct user is already
+                # persisted in the session and we don't need to continue.
 
-            # store the cookie on the request so the provider doesn't try to set
-            # a new one on every request
-            # We are seeing this user for the first time in this session, attempt
-            # to authenticate the user.
-            user = auth.authenticate(cookie_str=cookie_str)
+                logger.debug('shared auth token found, authenticating user')
+                # store the cookie on the request so the provider doesn't try to set
+                # a new one on every request
+                # We are seeing this user for the first time in this session, attempt
+                # to authenticate the user.
+                user = auth.authenticate(cookie_str=cookie_str)
 
-            if request.user.is_authenticated() and request.user == user:
-                return
+                if request.user.is_authenticated() and request.user == user:
+                    logger.debug('user is already properly authenticated')
+                    return
 
-            if user:
-                # User is valid.  Set request.user and persist user in the session
-                # by logging the user in.
-                request.user = user
-                auth.login(request, user)
+                if user:
+                    logger.debug('user exists, forwarding to backend')
+                    # User is valid.  Set request.user and persist user in the session
+                    # by logging the user in.
+                    request.user = user
+                    auth.login(request, user)
+                    logger.debug('user logged in, session is %s' %(request.session))
+                    request.session['SHARED_AUTH_USER'] = True
+                else:
+                    logger.debug('invalid user, redirecting to failure notice')
+                    redirect_to = getattr(settings, 'AUTHENTICATION_FAIL_REDIRECT_URL', None)
+                    if redirect_to:
+                        return HttpResponseRedirect(redirect_to)
             else:
-                redirect_to = getattr(settings, 'AUTHENTICATION_FAIL_REDIRECT_URL', None)
-                if redirect_to:
-                    return HttpResponseRedirect(redirect_to)
-        else:
-            if request.user.is_authenticated():
-                auth.logout(request)
+                if request.user.is_authenticated() \
+                        and hasattr(request, 'session') \
+                        and request.session.has_key('SHARED_AUTH_USER'):
+                    logger.debug('user is authenticated via shared_auth without a token, logging out')
+                    auth.logout(request)
+        except Exception, e:
+            logger.exception(e)
+            return response
 
 class SharedAuthProviderMiddleware(object):
     """
@@ -77,35 +91,38 @@ class SharedAuthProviderMiddleware(object):
         If the authenticated user disappears but the at cookie remains,
         the at cookie should be deleted.
         """
-        modify = False
-        if 'UPDATE_SHAREDAUTH_COOKIE' in dict(response.items()):
-            response.__delitem__('UPDATE_SHAREDAUTH_COOKIE')
-            modify = True
+        try:
+            modify = False
+            if 'UPDATE_SHAREDAUTH_COOKIE' in dict(response.items()):
+                response.__delitem__('UPDATE_SHAREDAUTH_COOKIE')
+                modify = True
 
-        if getattr(request, 'session', None) and \
-                hasattr(request, 'user') and \
-                request.user.is_authenticated() and \
-                (modify or not request.COOKIES.has_key(settings.COOKIE_NAME)):
+            if getattr(request, 'session', None) and \
+                    hasattr(request, 'user') and \
+                    request.user.is_authenticated() and \
+                    (modify or not request.COOKIES.has_key(settings.COOKIE_NAME)):
 
-            if request.session.get_expire_at_browser_close():
-                max_age = None
-                expires = None
-            else:
-                max_age = request.session.get_expiry_age()
-                expires_time = time.time() + max_age
-                expires = cookie_date(expires_time)
-            response.set_cookie(settings.COOKIE_NAME,
-                    SharedAuthBackend.getCookieStr(request.user),
-                    max_age=max_age,
-                    expires=expires,
-                    domain=settings.COOKIE_DOMAIN,
-                    path=settings.COOKIE_PATH,
-                    secure=settings.SECURE)
-        if getattr(request, 'session', None) and \
-                not (hasattr(request, 'user') and \
-                request.user.is_authenticated()) and \
-                request.COOKIES.has_key(settings.COOKIE_NAME):
-            response.delete_cookie(settings.COOKIE_NAME)
-        return response
-
+                if request.session.get_expire_at_browser_close():
+                    max_age = None
+                    expires = None
+                else:
+                    max_age = request.session.get_expiry_age()
+                    expires_time = time.time() + max_age
+                    expires = cookie_date(expires_time)
+                response.set_cookie(settings.COOKIE_NAME,
+                        SharedAuthBackend.getCookieStr(request.user),
+                        max_age=max_age,
+                        expires=expires,
+                        domain=settings.COOKIE_DOMAIN,
+                        path=settings.COOKIE_PATH,
+                        secure=settings.SECURE)
+            if getattr(request, 'session', None) and \
+                    not (hasattr(request, 'user') and \
+                    request.user.is_authenticated()) and \
+                    request.COOKIES.has_key(settings.COOKIE_NAME):
+                response.delete_cookie(settings.COOKIE_NAME)
+            return response
+        except Exception, e:
+            logger.exception(e)
+            return response
 
