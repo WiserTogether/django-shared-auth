4a6d534a73d4a2b05fb6455d8dd23a9e82537901
David Swenson <david.swenson@wisertogether.com>
2017-07-06 15:48:29 -0400

    Update middleware.py

--- django_shared_auth/middleware.py
+++ django_shared_auth/middleware.py
@@ -24,6 +24,19 @@ class SharedAuthConsumerMiddleware(object):
             'django_shared_auth.backends.SharedAuthBackend',
         )
     """
+    def __init__(self, get_response=None):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        response = None
+        if hasattr(self, 'process_request'):
+            response = self.process_request(request)
+        if not response:
+            response = self.get_response(request)
+        if hasattr(self, 'process_response'):
+            response = self.process_response(request, response)
+        return response
+
     def process_request(self, request):
         logger.debug('Starting SharedAuthProviderMiddleware.process_request')
         try:
@@ -79,6 +92,18 @@ class SharedAuthProviderMiddleware(object):
             ...
         )
     """
+    def __init__(self, get_response=None):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        response = None
+        if hasattr(self, 'process_request'):
+            response = self.process_request(request)
+        if not response:
+            response = self.get_response(request)
+        if hasattr(self, 'process_response'):
+            response = self.process_response(request, response)
+        return response
 
     def process_response(self, request, response):
         """
